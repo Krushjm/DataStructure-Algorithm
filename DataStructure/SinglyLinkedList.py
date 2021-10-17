221ea247d8b02feff4ab548884b50217c7c818ad
Krus <krushjm@outlook.com>
2021-10-17 20:27:57 +0800

    feat(ds): Add length attribute

--- DataStructure/SinglyLinkedList.py
+++ DataStructure/SinglyLinkedList.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import List, Optional
+from typing import List
 
 
 class SinglyListNode:
@@ -18,8 +18,9 @@ class SinglyLinkedList:
         Middle: [Peek] + O(1)
     """
     def __init__(self, values: List[int], mode: str = 'tail'):
-        self.head: Optional[SinglyListNode] = None
-        self.tail: Optional[SinglyListNode] = None
+        self.head = SinglyListNode(0)
+        self.tail = self.head
+        self.length = 0
         self.construct_list(values=values, mode=mode)
 
     def construct_list(self, values: List[int], mode: str):
@@ -28,54 +29,52 @@ class SinglyLinkedList:
 
         for v in values:
             node = SinglyListNode(v)
-
-            if self.head is None:
-                self.head, self.tail = node, node
+            if mode == 'tail':
+                self.add_tail(node)
+            elif mode == 'head':
+                self.add_head(node)
             else:
-                if mode == 'tail':
-                    self.add_tail(node)
-                elif mode == 'head':
-                    self.add_head(node)
-                else:
-                    raise ValueError('Wrong construct mode, only support "head" and "tail"!')
+                raise ValueError('Wrong construct mode, only support "head" and "tail"!')
 
     def add_tail(self, node):
         self.tail.next, self.tail = node, node
+        self.length += 1
 
     def add_head(self, node):
         node.next, self.head.next = self.head.next, node
         self.tail = node if self.head == self.tail else self.tail
+        self.length += 1
 
     def remove_head(self):
-        if self.head and self.head.next:
-            self.head.next, self.head = None, self.head.next
-        else:
-            self.head, self.tail = None, None
+        if not self.head == self.tail:
+            self.head.next = self.head.next.next
+            if self.head.next is None:
+                self.tail = self.head
+            self.length -= 1
 
     def remove_tail(self):
-        if self.head and self.head.next:
+        if not self.head == self.tail:
             node = self.head
             while node.next != self.tail:
                 node = node.next
             self.tail, node.next = node, None
-        else:
-            self.head, self.tail = None, None
+            self.length -= 1
 
     def traverse(self):
-        node = self.head
+        node = self.head.next
         while node:
             print(node.val)
             node = node.next
 
 
 if __name__ == '__main__':
-    sll = SinglyLinkedList([1, 2, 3, 4])
-    print(sll.head.val)
-    print(sll.tail.val)
+    sll = SinglyLinkedList([1, 2, 3, 4], 'head')
 
     sll.remove_head()
     sll.traverse()
+    print('length: ', sll.length)
 
     sll.remove_tail()
     sll.remove_tail()
     sll.traverse()
+    print('length: ', sll.length)
--- DataStructure/SinglyLinkedListWithoutDummyHead.py
+++ DataStructure/SinglyLinkedListWithoutDummyHead.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import List
+from typing import List, Optional
 
 
 class SinglyListNode:
@@ -18,8 +18,9 @@ class SinglyLinkedList:
         Middle: [Peek] + O(1)
     """
     def __init__(self, values: List[int], mode: str = 'tail'):
-        self.head = SinglyListNode(0)
-        self.tail = self.head
+        self.head: Optional[SinglyListNode] = None
+        self.tail: Optional[SinglyListNode] = None
+        self.length = 0
         self.construct_list(values=values, mode=mode)
 
     def construct_list(self, values: List[int], mode: str):
@@ -28,48 +29,59 @@ class SinglyLinkedList:
 
         for v in values:
             node = SinglyListNode(v)
-            if mode == 'tail':
-                self.add_tail(node)
-            elif mode == 'head':
-                self.add_head(node)
+
+            if self.head is None:
+                self.head, self.tail = node, node
+                self.length += 1
             else:
-                raise ValueError('Wrong construct mode, only support "head" and "tail"!')
+                if mode == 'tail':
+                    self.add_tail(node)
+                elif mode == 'head':
+                    self.add_head(node)
+                else:
+                    raise ValueError('Wrong construct mode, only support "head" and "tail"!')
 
     def add_tail(self, node):
         self.tail.next, self.tail = node, node
+        self.length += 1
 
     def add_head(self, node):
         node.next, self.head.next = self.head.next, node
         self.tail = node if self.head == self.tail else self.tail
+        self.length += 1
 
     def remove_head(self):
-        if not self.head == self.tail:
-            self.head.next = self.head.next.next
-            if self.head.next is None:
-                self.tail = self.head
+        if self.head and self.head.next:
+            self.head.next, self.head = None, self.head.next,
+            self.length -= 1
+        else:
+            self.head, self.tail, = None, None
 
     def remove_tail(self):
-        if not self.head == self.tail:
+        if self.head and self.head.next:
             node = self.head
             while node.next != self.tail:
                 node = node.next
-            self.tail, node.next = node, None
+            self.tail, node.next, = node, None
+            self.length -= 1
+        else:
+            self.head, self.tail = None, None
 
     def traverse(self):
-        node = self.head.next
+        node = self.head
         while node:
             print(node.val)
             node = node.next
 
 
 if __name__ == '__main__':
-    sll = SinglyLinkedList([1, 2, 3, 4], 'head')
-    print(sll.head.next.val)
-    print(sll.tail.val)
+    sll = SinglyLinkedList([1, 2, 3, 4])
 
     sll.remove_head()
     sll.traverse()
+    print('length: ', sll.length)
 
     sll.remove_tail()
     sll.remove_tail()
     sll.traverse()
+    print('length: ', sll.length)
